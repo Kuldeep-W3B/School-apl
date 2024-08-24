1d596ed1d7d88df76e26f238f4299d12abc70d7a
Kuldeep Singh <148358556+Kuldeep-W3B@users.noreply.github.com>
2024-08-24 17:21:36 +0530

    Update school.js

--- models/school.js
+++ models/school.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
+    host: "localhost",
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 5432 
+    database: "school_management",
+    port: 5432 
 });
 
 module.exports = pool;
